f92703c993d11fd25513b7416d9bb4720c4f30d6
pawel <plapacz6@gmail.com>
2022-09-25 14:12:02 +0200

    fixing logic and tests

--- js/bracket_match.js
+++ js/bracket_match.js
@@ -7,32 +7,47 @@ function is_bracket_match(str) {
 
   const re = /([\(\)])/g;
   const brackets = str.match(re);
-  
+ 
+  console.log({brackets})
+  if(brackets == undefined) //null)
+    return 0;  
+  if(brackets[0] === ')')
+    return 0;
+
   let nesting_level = 0;  
-  for(let i = 0;
-      i < ( brackets ? brackets.length : 0 ); 
-      i++){
-    console.log(brackets[i]);
-    if(brackets[i] == '('){
-      nesting_level++;
-    }
-    else{
-      nesting_level--;
-    }
+  for(let i = 0; i < brackets.length; i++){
+    //console.log(brackets[i]);
+    if(brackets[i] == '(')
+      nesting_level++;    
+    else 
+      nesting_level--;    
   }
-  
-  return nesting_level > 0 ? 0 : 1;
+  console.log({nesting_level}); 
+  return nesting_level === 0 ? 1 : 0;
 
 }   
 
 
 const tests = [
   ["(let x = 10; i < (y); (i++))", 1],
+  [")(let x = 10; i < (y); (i++))", 0],
   ["(let x = 10; i < (y); (i++)", 0],
   ["(let x = 10; i < ((y); (i++))", 0],
   ["a bcd e f(", 0],
   ["a bcd e f", 0],
 ];
+let test_pass = 0;
+let test_fail = 0;
 for(let i = 0; i < tests.length; i++){
-  console.log(is_bracket_match(tests[i][0]) === tests[i][1] );
+  if(is_bracket_match(tests[i][0]) === tests[i][1] ){
+    console.log("test pass");
+    test_pass++;
+  }
+  else{
+    console.log("test fail");
+    test_fail++;
+  } 
 }
+console.log({test_pass});
+console.log({test_fail});
+
