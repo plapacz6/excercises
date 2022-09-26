07577e493bb8c825ad0fc0e94e0e46feb3988a23
pawel <plapacz6@gmail.com>
2022-09-26 12:25:22 +0200

    separate test engine

---
+++ js/test_engine.js
@@ -0,0 +1,27 @@
+// const tests = [
+//   ["(let x = 10; i < (y); (i++))", 1],
+//   [")(let x = 10; i < (y); (i++))", 0],
+//   ["(let x = 10; i < (y); (i++)", 0],
+//   ["(let x = 10; i < ((y); (i++))", 0],
+//   ["a bcd e f(", 0],
+//   ["a bcd e f", 0],
+// ];
+/** 
+ * @param tests - array of tupes of function argument and expected function result value
+ */
+exports.run_tests = function  (tests, tested_function) {
+  let test_pass = 0;
+  let test_fail = 0;
+  for(let i = 0; i < tests.length; i++){
+    if(tested_function(tests[i][0]) === tests[i][1] ){
+      console.log("test pass");
+      test_pass++;
+    }
+    else{
+      console.log("test fail");
+      test_fail++;
+    } 
+  }
+  console.log({test_pass});
+  console.log({test_fail});
+}
