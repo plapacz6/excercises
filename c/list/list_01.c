c8836fb8df4a5c0ee3ade5c0947eb083296a3c1d
pawel <plapacz6@gmail.com>
2023-05-31 10:23:53 +0200

    format

--- c/list/list_01.c
+++ c/list/list_01.c
@@ -5,89 +5,73 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
- 
-typedef struct INode
+
+typedef struct Node
 {
     int val;
-    struct INode* next;
-} INode;
- 
+    struct Node* next;
+} Node;
+
 typedef struct List
 {
-
-    INode *first;
-    INode *last;
+    Node *first;
+    Node *last;
 } List;
- 
+
 List* List_new(void);
 void List_delete(List* il);
 void List_push_back(List* il, int val);
- 
 
- 
 List* List_new(void)
 {
-
     List* List = malloc(sizeof(List));
     if(!List) {
-      return NULL;
+        return NULL;
     }
     List->first = NULL;
     List->last = NULL;
     return List;
 }
- 
+
 void List_delete(List* const il)
 {
-
-   INode *tmp = NULL;
-   while(il->first){
-      tmp = il->first->next;
-      free(il->first);
-      il->first = tmp;
-   }
-   if(il){
-     free(il);     
-   }
+    Node *tmp = NULL;
+    while(il->first) {
+        tmp = il->first->next;
+        free(il->first);
+        il->first = tmp;
+    }
+    if(il) {
+        free(il);
+    }
 }
- 
+
 void List_push_back(List* const il, const int val)
 {
-
     if(!il->first) {
-      il->first = malloc(sizeof(INode));
-      if(!il->first){
-        exit(1);
-      }
-      il->first->next = NULL;
-      il->first->val = val;
-      il->last = il->first;
+        il->first = malloc(sizeof(Node));
+        if(!il->first) {
+            exit(1);
+        }
+        il->first->next = NULL;
+        il->first->val = val;
+        il->last = il->first;
     }
-    else{
-      il->last->next = malloc(sizeof(INode));
-      if(!il->last->next){
-        exit(1);
-      }      
-      il->last->next->val = val;
-      il->last->next->next = NULL;
-      il->last = il->last->next;
+    else {
+        il->last->next = malloc(sizeof(Node));
+        if(!il->last->next) {
+            exit(1);
+        }
+        il->last->next->val = val;
+        il->last->next->next = NULL;
+        il->last = il->last->next;
     }
 }
-char *text1 = "ala ma kota";
-char text2[] = "ola ma kota";
 
 int main(int argc, char* argv[static argc])
 {
- 
-    for(size_t i = 0; i < n; i++){
-      printf("[%c]", tbl_c[i]);
-    }
-    printf("\n");
-
-
     List* il = List_new();
- 
-    /* Dopisz pozostale funkcje i testy */
+
     assert(il != NULL);
     assert(il->first == NULL);
     assert(il->last == NULL);
@@ -102,7 +86,7 @@ int main(int argc, char* argv[static argc])
     assert(il->first->val == x);
     assert(il->last->next == NULL);
     assert(il->first->next != NULL);
-    
+
     assert(il->last->val == y);
     //printf("val: %d, y: %d\n", il->last->val, y);
 
@@ -119,7 +103,6 @@ int main(int argc, char* argv[static argc])
     assert(il->first->next != NULL);
 
     List_delete(il);
-
 
     return 0;
 }
