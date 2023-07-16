5fead53ce6b86f9d7f53ee79b8a4b21b0e7b8e06
pawel <plapacz6@gmail.com>
2023-07-16 13:32:31 +0200

    formating, solution3 in find_odd... WIP

--- c/find_odd_number_in_array.c
+++ c/find_odd_number_in_array.c
@@ -12,6 +12,7 @@ there is only one odd number in A
 #include <stdbool.h>
 #include <assert.h>
 #include <time.h>
+#include "list2direction/IList2d.h"
 
 int solution1(int A[], int N) {
     size_t j = 0;
@@ -37,9 +38,15 @@ int solution2(int A[], int N) {
     if(!pM) {
         return -1;
     }
+    size_t *oddidxs = malloc(((N / 2) + 1) * sizeof(size_t));
+    if(!oddidxs) {
+        free(pM);
+        return -1;
+    }
     size_t j = 0;
     size_t i = 0;
     size_t pMsize = 0;
+    size_t oddidxs_size = 0;
     int oddidx = 0;
     bool isodd = true;
 
@@ -47,7 +54,9 @@ int solution2(int A[], int N) {
         isodd = true;
         for(j = 0; j < pMsize; j++) {
             if(*(pM + j) == A[i]) {
-                isodd = false;
+                isodd = false;                
+                oddidx = oddidxs[oddidxs_size - 2];  //last + place for new
+                oddidxs_size--;
                 break;
             }
         }
@@ -55,55 +64,92 @@ int solution2(int A[], int N) {
             *(pM + pMsize) = A[i];
             pMsize++;
             oddidx = i;
+            oddidxs[oddidxs_size] = oddidx;            
+            oddidxs_size++;
         }
     }
     if(pM) {
         free(pM);
     }
+    if(oddidxs) {
+        free(oddidxs);
+    }
     return A[oddidx];
 }
 
-int main(){
-
-  srand(time(NULL));
-
-  enum {N1 = 1024, N2 = 10000};
-  for(size_t j = 0; j < 10; j++){
-    {
-      int tbl1[N1];    
-      for(size_t i = 0; i < N1/2; i++){
-        tbl1[i] = rand() % 1000;
-      }  
-      for(size_t i = 0; i < N1/2; i++){
-        tbl1[i + N1/2 + 1] = tbl1[i];
-      }
-      tbl1[N1/2] = 1009;
-
-      assert(solution1(tbl1, N1) == 1009);
-      assert(solution2(tbl1, N1) == 1009);
-      assert(solution1 (tbl1, N1) == solution2(tbl1, N1));
-      printf("solution1(): %d\n", solution1(tbl1, N1));
-      printf("solution1(): %d\n", solution2(tbl1, N1));
+int solution3(int A[], int N) {
+    IList2d* mem_list = IList2d_new();
+    if(!mem_list) {
+        return -1;
     }
-    {
-      int tbl1[N2];    
-      for(size_t i = 0; i < N2/4; i++){
-        tbl1[i] = rand() % 1000;
-      }  
-      for(size_t i = 0; i < N2/4; i++){
-        tbl1[i + (1 * N2/4) + 1] = tbl1[i];
-        tbl1[i + (2 * N2/4) + 1] = tbl1[i];
-        tbl1[i + (3 * N2/4) + 1] = tbl1[i];
-      }
-      tbl1[N2/4] = 1029;
 
-      assert(solution1(tbl1, N2) == 1029);
-      assert(solution2(tbl1, N2) == 1029);
-      assert(solution1 (tbl1, N2) == solution2(tbl1, N2));
-      printf("solution1(): %d\n", solution1(tbl1, N2));
-      printf("solution1(): %d\n", solution2(tbl1, N2));
+    size_t i = 0;
+    size_t pMsize = 0;
+    int oddidx = 0;
+    bool isodd = true;
+    IList2dNode *node = NULL;
+
+    for(i = 0; i < N; i++) {
+        isodd = true;
+        node = IList2d_find_node(A[i], mem_list->first);
+        if(node) {
+            isodd = false;
+            //oddidx = PREV_oddidx;
+        }
+        if(isodd) {
+            IList2d_push_back(mem_list, A[i]);
+            oddidx = i;
+        }
     }
-  }
+    IList2d_delete(mem_list);
+    return A[oddidx];
+}
 
-  return EXIT_SUCCESS;
+int main() {
+
+    srand(time(NULL));
+
+    enum {N1 = 1024, N2 = 10000};
+    for(size_t j = 0; j < 10; j++) {
+        {
+            int tbl1[N1];
+            for(size_t i = 0; i < N1/2; i++) {
+                tbl1[i] = rand() % 1000;
+            }
+            for(size_t i = 0; i < N1/2; i++) {
+                tbl1[i + N1/2 + 1] = tbl1[i];
+            }
+            tbl1[N1/2] = 1009;
+
+            //assert(solution1(tbl1, N1) == 1009);
+            assert(solution2(tbl1, N1) == 1009);
+            assert(solution3(tbl1, N1) == 1009);
+            //assert(solution1 (tbl1, N1) == solution2(tbl1, N1));
+            printf("solution1(): %d\n", solution1(tbl1, N1));
+            printf("solution2(): %d\n", solution2(tbl1, N1));
+            printf("solution3(): %d\n", solution3(tbl1, N1));
+        }
+        {
+            int tbl1[N2];
+            for(size_t i = 0; i < N2/4; i++) {
+                tbl1[i] = rand() % 1000;
+            }
+            for(size_t i = 0; i < N2/4; i++) {
+                tbl1[i + (1 * N2/4) + 1] = tbl1[i];
+                tbl1[i + (2 * N2/4) + 1] = tbl1[i];
+                tbl1[i + (3 * N2/4) + 1] = tbl1[i];
+            }
+            tbl1[N2/4] = 1029;
+
+            //assert(solution1(tbl1, N2) == 1029);
+            assert(solution2(tbl1, N2) == 1029);
+            assert(solution3(tbl1, N2) == 1029);
+            //assert(solution1 (tbl1, N2) == solution2(tbl1, N2));
+            printf("solution1(): %d\n", solution1(tbl1, N2));
+            printf("solution2(): %d\n", solution2(tbl1, N2));
+            printf("solution3(): %d\n", solution2(tbl1, N2));
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
