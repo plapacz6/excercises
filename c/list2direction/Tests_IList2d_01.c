5460f26867a92c95ef033872353f3448186bcaa1
pawel <plapacz6@gmail.com>
2023-07-16 12:08:08 +0200

    IList2d_find_node(), IList2d_remove_node()

--- c/list2direction/Tests_IList2d_01.c
+++ c/list2direction/Tests_IList2d_01.c
@@ -48,6 +48,26 @@ int main(int argc, char* argv[static argc])
     assert(il->first->prev == NULL);
     assert(il->last->prev == il->first->next->next);
 
+    y = 12;
+    IList2dNode *node =
+        IList2d_find_node(12, il->first);
+    assert(node != NULL);
+    assert(node->val == 12);
+    assert(node->next->val == 13);
+
+    IList2dNode *node2 = IList2d_remove_node(il, node);
+    assert(node2 != NULL);
+    assert(node2 = node);
+
+    IList2dNode *node3 = IList2d_find_node(11, il->first);
+    assert(node3 != NULL);
+    assert(node3->val == 11);
+    assert(node3->next->val == 13);
+    free(node);
+    node = NULL;
+    node = IList2d_find_node(12, il->first);
+    assert(node == NULL);
+
     IList2d_delete(il);
 
     return 0;
