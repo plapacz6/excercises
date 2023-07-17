ec330c876934e1f4dd86c3b50a2accc90e7d3acd
pawel <plapacz6@gmail.com>
2023-07-17 16:49:24 +0200

    formatting

--- c/find_missing_el_in_array.c
+++ c/find_missing_el_in_array.c
@@ -8,34 +8,34 @@ only one value is missing
 #include <assert.h>
 #include <time.h>
 
-int cmp_int(const void* a, const void* b){
+int cmp_int(const void* a, const void* b) {
     return *(int*)a > *(int*)b;
 }
 int solution(int A[], int N) {
     if(!A || N < 1) {
         return 1;
     }
-    if(N == 1){
-        if(A[0] == 1){
+    if(N == 1) {
+        if(A[0] == 1) {
             return 2;
         }
         else {
             return 1;
         }
-    }    
+    }
     qsort(A, N, sizeof(int), cmp_int);
-    
+
     // for(size_t j = 0; j < N; j++){
     //     printf("[%d]", A[j]);
     // }
     // printf("\n");
 
-    if(A[0] != 1){
+    if(A[0] != 1) {
         return 1;
     }
     size_t i = 0;
-    for( ; i < N - 1; ++i){
-        if(A[i + 1] - A[i] > 1){
+    for( ; i < N - 1; ++i) {
+        if(A[i + 1] - A[i] > 1) {
             break;
         }
     }
@@ -45,7 +45,7 @@ int solution(int A[], int N) {
 void shuffle_int(int* tbl, size_t size) {
     int tmp;
     size_t idx;
-    for(size_t i = 0; i < size; ++i){
+    for(size_t i = 0; i < size; ++i) {
         idx = rand() % size;
         tmp = tbl[idx];
         tbl[idx] = tbl[i];
@@ -58,15 +58,15 @@ void print_tbl(int *tbl, size_t size) {
     }
     printf("%c", '\n');
 }
-int main(){
+int main() {
 
-        assert(solution(NULL, 0) == 1);
-        int tbl1[1] = {2};
-        assert(solution(tbl1, 1) == 1);
-        tbl1[0] = 1;
-        assert(solution(tbl1, 1) == 2);
+    assert(solution(NULL, 0) == 1);
+    int tbl1[1] = {2};
+    assert(solution(tbl1, 1) == 1);
+    tbl1[0] = 1;
+    assert(solution(tbl1, 1) == 2);
 
-    srand(time(NULL));    
+    srand(time(NULL));
     for(size_t k = 0; k < 400; ++k) {
         size_t size = rand() % 100000;
         int *tbl = malloc(size * sizeof(int));
@@ -76,7 +76,7 @@ int main(){
         for(size_t i = 0; i < size; ++i) {
             tbl[i] = i + 1;
         }
-        shuffle_int(tbl, size);        
+        shuffle_int(tbl, size);
         int expected = tbl[size - 1];
         int result = solution(tbl, size - 1);
 
